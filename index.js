5aa69f29e476d193a9579dc0ed28640f9e625241
lukehan1128 <lukehan1128@gmail.com>
2022-08-16 22:28:30 +0900

    Update js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.onload = function(){
 
-  var type = navigator.appName;
-  var lang;
-
-  if(type=='Netscape')
-    lang = navigator.language
-  else
-    lang = navigator.userLanguage
-
-  lang = lang.substr(0,2)
-
-  if(lang != 'ko')
-    document.querySelector('div.row.korea').style.display = 'flex';
-  else
+//  var type = navigator.appName;
+//  var lang;
+//
+//  if(type=='Netscape')
+//    lang = navigator.language
+//  else
+//    lang = navigator.userLanguage
+//
+//  lang = lang.substr(0,2)
+//
+//  if(lang == 'ko')
+//    document.querySelector('div.row.korea').style.display = 'flex';
+//  else
     document.querySelector('div.row.english').style.display = 'flex';
 }
