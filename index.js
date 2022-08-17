219b2a2226e831f14a3b3134e5ceadcfe38ccf89
lukehan1128 <lukehan1128@gmail.com>
2022-08-17 20:22:58 +0900

    Update add map and location and account area

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 window.onload = function(){
-
+  // check location - Korea or not
   var type = navigator.appName;
   var lang;
 
-  if(type=='Netscape')
+  if(type == 'Netscape')
     lang = navigator.language
   else
     lang = navigator.userLanguage
 
   lang = lang.substr(0,2)
 
-  if(lang != 'ko')
+  if(lang == 'ko'){
     document.querySelector('.korea').style.display = 'block';
-  else
+    get_account();
+  }
+  else{
     document.querySelector('.korea').style.display = 'none';
+  }
+}
+
+function get_account(){
+  // get info
+  var infoVal = '국민은행';
+
+  // set copy account button
+  var info = document.querySelector('#info');
+  info.value = infoVal;
+  info.readOnly = true;
+  document.querySelector('#infoBtn').disabled = false;
 }
 
 function copy_to_clipboard(){
+  var info = document.querySelector('#info').value;
   var cpTxt = document.getElementById('info');
   cpTxt.select();
   document.execCommand('Copy');
-  alert('복사하였습니다.');
+
+  alert(info + '\n\n복사하였습니다.');
 }
