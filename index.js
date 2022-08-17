96cf1ede34b0601e3bc7dbf0979b0c79d22fd09a
LukeHan <LukeHan1128@gmail.com>
2022-08-17 14:30:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 window.onload = function(){
 
-//  var type = navigator.appName;
-//  var lang;
-//
-//  if(type=='Netscape')
-//    lang = navigator.language
-//  else
-//    lang = navigator.userLanguage
-//
-//  lang = lang.substr(0,2)
-//
-//  if(lang == 'ko')
-//    document.querySelector('div.row.korea').style.display = 'flex';
-//  else
-    document.querySelector('div.row.english').style.display = 'flex';
+  var type = navigator.appName;
+  var lang;
+
+  if(type=='Netscape')
+    lang = navigator.language
+  else
+    lang = navigator.userLanguage
+
+  lang = lang.substr(0,2)
+
+  if(lang != 'ko')
+    document.querySelector('.korea').style.display = 'block';
+  else
+    document.querySelector('.korea').style.display = 'none';
+}
+
+function copy_to_clipboard(){
+  var cpTxt = document.getElementById('info');
+  cpTxt.select();
+  document.execCommand('Copy');
+  alert('복사하였습니다.');
 }
